2d76ee73f8bb1e8b2d86fc41daa68e38ec8f14db
Miguel Pignatelli <mp@ebi.ac.uk>
2014-09-16 11:56:14 +0100

    coveralls included

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,35 +8,66 @@ module.exports = function (grunt) {
 	    files : ['<%= jshint.files %>'],
 	    tasks : ['jshint']
 	},
+	mochacov : {
+	    coverage : {
+		options : {
+		    coveralls : true
+		}
+	    },
+	    test : {
+		options : {
+		    reporter : "spec"
+		}
+	    },
+	    options : {
+		files : 'test/**/*.js'
+	    }
+	    // options : {
+	    // 	reporter : 'html-cov',
+	    // 	require :  ['assert']
+	    // },
+	    // all : 'test/**/*.js'
+	},
+
 	mochaTest : {
 	    test : {
-		options: {
-		    reporter: 'spec',
-		    require : 'coverage/blanket'
-		},
-		src: ['test/**/*.js']
-	    },
-	    'html-cov' : {
 		options : {
-		    reporter : 'html-cov',
-		    quiet : true,
-		    captureFile : 'coverage/coverage.html'
-		},
-		src : ['test/**/*.js']
-	    },
-	    'travis-cov' : {
-		options : {
-		    reporter : 'travis-cov',
+		    reporter : "spec"
 		},
 		src : ['test/**/*.js']
 	    }
 	}
+
+	// mochacov : {
+	//     test : {
+	// 	options: {
+	// 	    reporter: 'html-cov',
+	// 	    require : ['assert']
+	// 	},
+	// 	src: ['test/**/*.js']
+	//     },
+	//     'html-cov' : {
+	// 	options : {
+	// 	    reporter : 'html-cov',
+	// 	    quiet : true,
+	// 	    captureFile : 'coverage/coverage.html'
+	// 	},
+	// 	src : ['test/**/*.js']
+	//     },
+	//     'travis-cov' : {
+	// 	options : {
+	// 	    reporter : 'travis-cov',
+	// 	},
+	// 	src : ['test/**/*.js']
+	//     }
+	// }
     });
 
     grunt.loadNpmTasks ('grunt-contrib-jshint');
-    grunt.loadNpmTasks ('grunt-contrib-watch');
     grunt.loadNpmTasks ('grunt-mocha-test');
+    grunt.loadNpmTasks ('grunt-mocha-cov');
 
-    grunt.registerTask ('default', ['jshint', 'mochaTest']);
-    grunt.registerTask ('test', ['jshint', 'mochaTest']);
+    grunt.registerTask ('coverage', ['mochacov:coverage']);
+    grunt.registerTask ('default', ['jshint', 'mochaTest', 'mochacov']);
+    grunt.registerTask ('test', ['jshint', 'mochaTest', 'mochacov']);
 };
